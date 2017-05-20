d07f37e08804218162e60bdb02ae2c8f8b1b65c6
Andreas Karlsson <sprawl@onlajn.nu>
2017-05-20 23:50:58 +0200

    added load snapshot on hydrate and storing snapshot on save

--- test/aggregates/location.js
+++ test/aggregates/location.js
@@ -1,10 +1,11 @@
-var util=require('util');
+var util = require('util');
 var Aggregate = require('../..').Aggregate;
 var Promise = require('bluebird');
 
-var Location = function(commandSink, eventHandler) {
+var Location = function (commandSink, eventHandler) {
   this.id = 1
-	Aggregate.call(this, commandSink, eventHandler);
+  Aggregate.call(this, commandSink, eventHandler);
+  this._state = {};
 }
 
 util.inherits(Location, Aggregate);
@@ -13,60 +14,61 @@ util.inherits(Location, Aggregate);
 
 // --------- CHANGE NAME
 
-Location.prototype.changeName = function(newName) {
-	return this._sink({type:'location.change_name.command', payload:newName, aggregateId: 1 });
+Location.prototype.changeName = function (newName) {
+  return this._sink({ type: 'location.change_name.command', payload: newName, aggregateId: 1 });
 };
 
-Location.prototype.processChangeName = function(command) {
-	return this._apply({type:'location.changed_name.event', payload:command.payload, aggregateId: 1}, true);
+Location.prototype.processChangeName = function (command) {
+  return this._apply({ type: 'location.changed_name.event', payload: command.payload, aggregateId: 1 }, true);
 };
 
-Location.prototype.applyChangedName = function(event) {
-	//change local state if necessary for validation
+Location.prototype.applyChangedName = function (event) {
+  //change local state if necessary for validation
+  this._state.name = event.payload;
 };
 
 
 
 // --------- CHANGE NAME (PROMISE)
 
-Location.prototype.changeNameAsync = function(newName) {
+Location.prototype.changeNameAsync = function (newName) {
   var promise = new Promise(function (resolve, reject) {
     setTimeout(function () {
-      resolve({type: 'location.change_name.command', payload: newName, aggregateId: 1 })
+      resolve({ type: 'location.change_name.command', payload: newName, aggregateId: 1 })
     }, 50)
   });
-	return this._sink(promise);
+  return this._sink(promise);
 };
 
-Location.prototype.processChangeNameAsync = function(command) {
-	return this._apply({type:'location.changed_name.event', payload:command.payload, aggregateId: 1}, true);
+Location.prototype.processChangeNameAsync = function (command) {
+  return this._apply({ type: 'location.changed_name.event', payload: command.payload, aggregateId: 1 }, true);
 };
 
-Location.prototype.applyChangedNameAsync = function(event) {
-	//change local state if necessary for validation
+Location.prototype.applyChangedNameAsync = function (event) {
+  this._state.name = event.payload;
 };
 
 
 
 // --------- FAIL NAME
 
-Location.prototype.failName = function(newName) {
-	return this._sink({type:'location.fail_name.command', payload:newName, aggregateId: 1 });
+Location.prototype.failName = function (newName) {
+  return this._sink({ type: 'location.fail_name.command', payload: newName, aggregateId: 1 });
 };
 
-Location.prototype.processFailName = function(command) {
+Location.prototype.processFailName = function (command) {
   var self = this;
-  if(command.payload === 'fail early') {
+  if (command.payload === 'fail early') {
     throw new Error('Failing early');
   }
-	return new Promise(function(resolve, reject) {
-    self._apply({type:'location.changed_name.event', payload:command.payload, aggregateId: 1}, true);
+  return new Promise(function (resolve, reject) {
+    self._apply({ type: 'location.changed_name.event', payload: command.payload, aggregateId: 1 }, true);
     reject(new Error('uh oh'))
   });
 };
 
-Location.prototype.applyFailedName = function(event) {
-	//change local state if necessary for validation
+Location.prototype.applyFailedName = function (event) {
+  //change local state if necessary for validation
 };
 
 
