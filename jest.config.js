11f0669cb4a6c58db3c78cdfbc0e49f0aa2dc610
Kenny Ek <ekkenny@hotmail.com>
2022-08-17 23:18:03 +0200

    Replace build tools & add TS support
    Replace Babel with SWC.
    
    Add tsconfig, convert jest-setup to TS.
    
    Update npm scripts.
    
    Add setup for lint and formatting.
    
    Add `.npmignore` to prevent publishing test files.

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
+/** @type { import('@jest/types').Config.InitialOptions } */
 module.exports = {
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
+  transform: {
+    '^.+\\.[t|j]s$': '@swc/jest'
+  },
   testMatch: [
     '**/?(*.)+(spec|test).[jt]s?(x)'
   ],
+  moduleFileExtensions: ['ts', 'js'],
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   testTimeout: 2000,
-  transform: {
-    '^.+\\.[t|j]s$': 'babel-jest'
-  },
   setupFiles: [
-    './jest-setup.js'
+    './jest-setup.ts'
   ]
 };
